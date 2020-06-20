9691efc2bb3b8bf6dfe8fb28af4145438635737c
LiYZhe <liyunzhe015@163.com>
2020-06-20 14:04:37 +0800

    'feature'

---
+++ src/components/homeRecommend/HomeRecommend.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class='recommend'>
+        <div class='item' v-for='item in recommendList()'>
+            <a href="">
+                <img :src="item.picUrl" alt="">
+                <div class='title'>{{item.title}}</div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeRecommend',
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        recommendList:Function
+    },
+    created() {
+        console.log(this.recommendList())
+    }
+}
+</script>
+<style scoped>
+    .recommend {
+        display: flex;
+    }
+    .item {
+        flex: 1;
+        text-align: center;
+    }
+    a {
+        text-decoration: none;
+    }
+    img {
+        width: 50px;
+    }
+    .title {
+        margin-bottom: -15px;
+    }
+</style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class='navbar'>
+        <div class='left'><slot name='left'></slot></div>
+        <div class='center'><slot name='center'></slot></div>
+        <div class='right'><slot name='right'></slot></div>
+    </div>
+</template>
+
+<script>
+const data=[
+    {url:''},
+    {url:''},
+    {url:''},
+    {url:''},
+    {url:''},
+    {url:''},
+    {url:''},
+    {url:''},
+    {url:''},
+]
+export default {
+    name:'Navbar',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style scoped>
+    .navbar {
+        display: flex;
+        position: fixed;
+        left: 0;
+        top: 0;
+        right: 0;
+        line-height: 44px;
+        height:44px;
+        color:white;
+        box-shadow: 0px 1px 1px rgba(100,100,100,0.1);
+    }
+    .left {
+        width: 60px;
+        /* background-color: red; */
+    }
+    .center{
+        flex: 1;
+        /* background-color: blue; */
+        text-align: center;
+    }
+    .right {
+        width: 60px;
+                /* background-color: red; */
+
+    }
+</style>
