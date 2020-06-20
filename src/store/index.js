15cbeda57804f259e34e68f02747c96dd7a10637
LiYZhe <liyunzhe015@163.com>
2020-06-20 14:04:18 +0800

    'feature'

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data:[
+      {
+        picUrl:"https://s10.mogucdn.com/mlcdn/c45406/200309_39fl50k505ij280hlajj3d73g2fld_224x224.png",
+        title:'上衣'
+      },{
+        picUrl:'https://s10.mogucdn.com/mlcdn/c45406/200309_3k62ck469f3j35afjd7j746ge9d31_224x224.png',
+        title:'裙子'
+      },{
+        picUrl:'https://s10.mogucdn.com/mlcdn/c45406/200309_1g7836gh88clid875e2fdl86l91jg_224x224.png',
+        title:'裤子'
+      },{
+        picUrl:'https://s10.mogucdn.com/mlcdn/c45406/200309_67j08ai5cj5fgilbfa3g4kikf1f2j_224x224.png',
+        title:'套装'
+      },{
+        picUrl:'https://s10.mogucdn.com/mlcdn/c45406/200309_4lka19gk484c6eh15f4lebc31326b_224x224.png',
+        title:'女鞋'
+      }
+    ]
   },
   mutations: {
   },
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,35 @@
 <!--  -->
 <template>
-<div>
-    我是首页
-</div>
+  <div id='home'>
+      <Navbar class='homeNav'><div slot='center'>购物车</div></Navbar>
+      <HomeRecommend class="content" :recommendList='recommendList'></HomeRecommend>
+  </div>
 </template>
 
 <script>
+import Navbar from '../../components/navbar/Navbar'
+import HomeRecommend from '../../components/homeRecommend/HomeRecommend'
 export default {
   name:'Home',
   data() {
     return {
-      
+      recommendList(){
+        return this.$store.state.data
+      }
     }
+  },
+  components:{
+    Navbar,
+    HomeRecommend
   },
 }
 </script>
 
 <style scoped>
+    .homeNav{
+      background-color: pink;
+    }
+    .content{
+      margin-top: 44px;
+    }
 </style>
